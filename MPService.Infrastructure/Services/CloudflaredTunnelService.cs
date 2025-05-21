a65a7872bd50bebe098ea9afcf93d907379a35c4
jamesxddev <jamescabral.dev@gmail.com>
2025-05-21 11:01:01 +0800

    Cleanup: Remove unused using directives
    Removed several unnecessary `using` directives from the
    `CloudflaredTunnelService.cs` file to enhance code
    readability and maintainability.

--- MPService.Infrastructure/Services/CloudflaredTunnelService.cs
+++ MPService.Infrastructure/Services/CloudflaredTunnelService.cs
@@ -1,12 +1,6 @@
 ﻿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MPService.Infrastructure.Services
 {
